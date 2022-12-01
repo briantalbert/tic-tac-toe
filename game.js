1bc539cf152301345761ebfab753c15015759cb7
Brian Talbert <BrianLTalbert@gmail.com>
2022-12-01 11:20:13 -0500

    Add name selector, footer

--- game.js
+++ game.js
@@ -1,74 +1,104 @@
 const Player = (playerName, letter) => {
     let name = playerName
-    
     return {name, letter}
 }
 
 const Game = (function() {
+    //create players
+    const player1 = Player(document.getElementById('player1').value ? document.getElementById('player1').value : 'Player One', 'X')
+    const player2 = Player(document.getElementById('player2').value ? document.getElementById('player2').value : 'Player Two', 'O')
+    const players = [player1, player2]
+    var playerIdx = 1
+
+    //check for win condition
     const gameOver = (board) => {
-        //check verticals
-        if (board[0][0] == board[1][0] == board[2][0] ||
-            board[0][1] == board[1][1] == board[2][1] ||
-            board[0][2] == board[1][2] == board[2][2]) {
+        if (board[0][0] != '' && board[0][0] == board[1][0] && board[1][0] == board[2][0] ||
+            board[0][1] != '' && board[0][1] == board[1][1] && board[1][1] == board[2][1] ||
+            board[0][2] != '' && board[0][2] == board[1][2] && board[1][2] == board[2][2]) {
+                //vertical column 3 in a row
                 return true
-            }
+        } else if (board[0][0] != '' && board[0][0] == board[0][1] && board[0][1] == board[0][2] ||
+        board[1][0] != '' && board[1][0] == board[1][1] && board[1][1] == board[1][2] ||
+        board[2][0] != '' && board[2][0] == board[2][1] && board[2][1] == board[2][2]) {
+            //horizontal row 3 in a row
+            return true
+        } else if (board[0][0] != '' && board[0][0] == board[1][1] && board[1][1] == board[2][2] ||
+        board[0][2] != '' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            //diagonal 3 in a row
+            return true
+        } else {
+            return false
+        }
+      
     }
+
+    const nextPlayer = () => {
+        playerIdx = (playerIdx + 1) % 2
+        return players[playerIdx]
+    }
+
+    return {gameOver, players, nextPlayer}
 })();
 
 const GameBoard = (function() {
+    const players = Game.players
+    var currentPlayer = Game.nextPlayer()
+
     var board = [['','',''],
                  ['','',''],
                  ['','','']]
+
+    const makeBoard = (function() {
+        boardDiv = document.querySelector('.board')
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const gameSquare = document.createElement('div')
+                gameSquare.setAttribute('data-row', i)     
+                gameSquare.setAttribute('data-col', j)
+                gameSquare.classList.add('square')
+
+                gameSquare.addEventListener('mousedown', () => {
+                    gameSquare.classList.add('clicked')
+                    if (gameSquare.textContent == '') {
+                        updateBoard(gameSquare, currentPlayer.letter)
+                        //updateBoard(gameSquare.getAttribute('data-row'), gameSquare.getAttribute('data-col'), currentPlayer.letter) //begin with GameBoard.?
+                        //gameSquare.textContent = currentPlayer.letter
+                        if (!(Game.gameOver(board))) {
+                            currentPlayer = Game.nextPlayer()
+                        } else {
+                            const footerText = document.querySelector('.footer-text')
+                            //alert(footerText)
+                            footerText.textContent = currentPlayer.name + " wins!"
+                        }
+                    }
+                })
+
+                gameSquare.addEventListener('mouseup', () => {
+                    gameSquare.classList.remove('clicked')
+                })
+                boardDiv.appendChild(gameSquare)
+            }
+        }
+    })();
     
-    const updateBoard = (row, col, letter) => {
-
-        board[row][col] = letter;
-    }
-
-    const showBoardConsole = () => {
-        board.forEach(row => {
-            console.log(row[0] + '|' + row[1] + '|' + row[2])
-        });
+    const updateBoard = (gameSquare, letter) => {
+        board[gameSquare.getAttribute('data-row')][gameSquare.getAttribute('data-col')] = letter;
+        gameSquare.textContent = currentPlayer.letter
     }
 
     const getBoard = () => {
         return board;
     }
 
-    return {showBoardConsole, updateBoard, getBoard}
+    const resetBoard = () => {
+        let squares = document.querySelectorAll('.square')
+        squares.forEach(box => {
+            console.log(box)
+            box.textContent = ''
+            GameBoard.updateBoard(box, '')
+        });
+    }
+
+    return {updateBoard, getBoard, resetBoard}
     
 })();
-
-
-boardDiv = document.querySelector('.board')
-for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        const gameSquare = document.createElement('div')
-        gameSquare.setAttribute('row', i)     
-        gameSquare.setAttribute('col', j)
-        gameSquare.classList.add('square')
-
-        gameSquare.addEventListener('mousedown', () => {
-            gameSquare.classList.add('clicked')
-            if (gameSquare.textContent == '') {
-                GameBoard.updateBoard(gameSquare.getAttribute('row'), gameSquare.getAttribute('col'), currentPlayer.letter)
-                gameSquare.textContent = currentPlayer.letter
-                currentPlayerIdx = (currentPlayerIdx + 1) % 2
-                currentPlayer = players[currentPlayerIdx]
-            }
-        })
-
-        gameSquare.addEventListener('mouseup', () => {
-            gameSquare.classList.remove('clicked')
-        })
-        boardDiv.appendChild(gameSquare)
-    }
-}
-
-const player1 = Player('Brian', 'X')
-const player2 = Player('Laura', 'O')
-const players = [player1, player2]
-let currentPlayerIdx = 0
-let currentPlayer = players[currentPlayerIdx]
-
-
